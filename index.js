329a6ab4440a805ee23c0b5031c518941f099af7
jarombyui <mcamposro93@ucvvirtual.edu.pe>
2024-06-05 15:55:49 -0500

    third commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // ESTE ES UN ARCHIVO DE PRUEBA
 //COMENTARIO 2
+
+//SUBIR PROYECTO A GIT HUB
+
+///repositorio local
+// git init
+// git add .
+// git commit -m "first commit"
+// gir remote add origin 'url del reposotorio'
+// git push -u origin master
+
+//para subir nuevos cambios a un repositorio
+
+// git add .
+// git commit -m "secod commit"
+// git push origin master
